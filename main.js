8b4b74f39ffea59c56440fef79c622af44053a92
Jerry Thatcher <t.jerry55011@gmail.com>
2018-12-14 16:29:33 -0600

    Started assignment, working on getting text to display in DOM

---
+++ main.js
@@ -0,0 +1,15 @@
+$(document).ready(readyNow);
+
+class Employee {
+    constructor(firstName, lastName, employeeId, employeeTitle, annualSalary){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.employeeId = employeeId;
+        this.employeeTitle = employeeTitle;
+        this.annualSalary = annualSalary;
+    }
+};
+
+function readyNow(){
+    
+};

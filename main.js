0d6ee022823acf771131ac421ee32ed67295d3cc
Jerry Thatcher <t.jerry55011@gmail.com>
2018-12-16 14:45:27 -0600

    Got undefined to return in log and dom

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
-$(document).ready(readyNow);
-let val = $('#textbox').val();
-function readyNow(){
-    $('#employeeList').append('<li>'+val+'</li>');
-};
+let employees = [];
+
+$(document).ready(function(){
+    $('#submit').on('click', function(){
+        let newEmployee = new Employee($('#firstName').val(), $('#lastName').val(),
+         $('#employeeId').val(), $('#employeeTitle').val(), $('#annualSalary').val(), );
+         employees.push(newEmployee);
+         updateEmployees();
+    })
+})
+
+function updateEmployees(){
+    let outputElement = $('#employeeList');
+    outputElement.empty();
+    for(employee of employees){
+        outputElement.append('<li>' + employee.firstName + '</li>')
+        console.log(employee)
+    }
+}
 
 class Employee {
     constructor(firstName, lastName, employeeId, employeeTitle, annualSalary){
